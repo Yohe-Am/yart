b2b1761796963de64b2d8080a30ee5591fd8fb8d
Yohe-Am <yoftie3@gmail.com>
2020-05-06 10:33:53 +0300

    implemented rays;

--- src/Math.rs
+++ src/Math.rs
@@ -4,58 +4,58 @@ use std::f64::EPSILON;
 pub type Num = f64;
 
 #[derive(PartialEq, Debug, Clone, Copy)]
-pub struct Vector3 {
+pub struct Vec3 {
     pub x: Num,
     pub y: Num,
     pub z: Num,
 }
 
 #[allow(dead_code)]
-impl Vector3 {
-    const EPSILON: Vector3 = Vector3 {
+impl Vec3 {
+    const EPSILON: Vec3 = Vec3 {
         x: EPSILON,
         y: EPSILON,
         z: EPSILON,
     };
 
-    pub fn new(x: Num, y: Num, z: Num) -> Vector3 {
-        Vector3 { x, y, z }
+    pub fn new(x: Num, y: Num, z: Num) -> Vec3 {
+        Vec3 { x, y, z }
     }
 
-    pub fn zero() -> Vector3 {
-        Vector3 {
+    pub fn zero() -> Vec3 {
+        Vec3 {
             x: 0.0,
             y: 0.0,
             z: 0.0,
         }
     }
 
-    pub fn one() -> Vector3 {
-        Vector3 {
+    pub fn one() -> Vec3 {
+        Vec3 {
             x: 1.0,
             y: 1.0,
             z: 1.0,
         }
     }
 
-    pub fn unit_x() -> Vector3 {
-        Vector3 {
+    pub fn unit_x() -> Vec3 {
+        Vec3 {
             x: 1.0,
             y: 0.0,
             z: 0.0,
         }
     }
 
-    pub fn unit_y() -> Vector3 {
-        Vector3 {
+    pub fn unit_y() -> Vec3 {
+        Vec3 {
             x: 0.0,
             y: 1.0,
             z: 0.0,
         }
     }
 
-    pub fn unit_z() -> Vector3 {
-        Vector3 {
+    pub fn unit_z() -> Vec3 {
+        Vec3 {
             x: 0.0,
             y: 0.0,
             z: 1.0,
@@ -69,15 +69,15 @@ impl Vector3 {
         Num::sqrt(self.magnitude_squared())
     }
 
-    pub fn unit_vector(self) -> Vector3 {
+    pub fn unit_vector(self) -> Vec3 {
         self / self.magnitude()
     }
 
-    pub fn dot(self, other: Vector3) -> Num {
+    pub fn dot(self, other: Vec3) -> Num {
         (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
     }
-    pub fn cross(self, other: Vector3) -> Vector3 {
-        Vector3 {
+    pub fn cross(self, other: Vec3) -> Vec3 {
+        Vec3 {
             x: (self.y * other.z) - (self.z * other.y),
             y: (self.z * other.x) - (self.x * other.z),
             z: (self.x * other.y) - (self.y * other.x),
@@ -85,17 +85,17 @@ impl Vector3 {
     }
 }
 
-impl std::fmt::Display for Vector3 {
+impl std::fmt::Display for Vec3 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {} {}", self.x, self.y, self.z)
     }
 }
 
-impl Add for Vector3 {
+impl Add for Vec3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Vector3 {
+        Vec3 {
             x: self.x + other.x,
             y: self.y + other.y,
             z: self.z + other.z,
@@ -103,23 +103,23 @@ impl Add for Vector3 {
     }
 }
 
-impl Sub for Vector3 {
+impl Sub for Vec3 {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
-        Vector3 {
+        Vec3 {
             x: self.x - other.x,
             y: self.y - other.y,
             z: self.z - other.z,
         }
     }
 }
-impl Mul for Vector3 {
+impl Mul for Vec3 {
     //TODO: replace this with something more explicit?
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        Vector3 {
+        Vec3 {
             x: self.x * other.x,
             y: self.y * other.y,
             z: self.z * other.z,
@@ -127,11 +127,11 @@ impl Mul for Vector3 {
     }
 }
 
-impl Mul<Num> for Vector3 {
+impl Mul<Num> for Vec3 {
     type Output = Self;
 
     fn mul(self, num: Num) -> Self {
-        Vector3 {
+        Vec3 {
             x: self.x * num,
             y: self.y * num,
             z: self.z * num,
@@ -139,7 +139,7 @@ impl Mul<Num> for Vector3 {
     }
 }
 
-impl Div<Num> for Vector3 {
+impl Div<Num> for Vec3 {
     type Output = Self;
 
     fn div(self, num: Num) -> Self {
@@ -153,27 +153,27 @@ mod tests {
 
     #[test]
     fn test_magnitude() {
-        let unit_vector = Vector3::one().unit_vector();
+        let unit_vector = Vec3::one().unit_vector();
         assert_eq!(unit_vector.magnitude(), 1.00);
     }
 
     #[test]
     fn test_add_sub() {
-        let sum = Vector3::one() + Vector3::one();
-        let difference = Vector3::new(2.0, 2.0, 2.0) - sum;
+        let sum = Vec3::one() + Vec3::one();
+        let difference = Vec3::new(2.0, 2.0, 2.0) - sum;
         assert!(difference.magnitude().abs() <= EPSILON);
     }
 
     #[test]
     fn test_dot() {
-        assert_eq!(Vector3::unit_x().dot(Vector3::unit_y()), 0.0);
+        assert_eq!(Vec3::unit_x().dot(Vec3::unit_y()), 0.0);
     }
 
     #[test]
     fn test_cross() {
         assert_eq!(
-            Vector3::unit_x().cross(Vector3::unit_y()),
-            Vector3::unit_z()
+            Vec3::unit_x().cross(Vec3::unit_y()),
+            Vec3::unit_z()
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,69 @@
-use crate::math::{Num, Vector3};
-use std::fs;
+use crate::math::{Num, Vec3};
 
 mod math;
 
 fn main() {
-    fs::write("hello_ray.ppm", hello_ray().as_bytes()).unwrap();
+    std::fs::write("hello_sky.ppm", hello_ray().as_bytes()).unwrap();
 }
 
 fn hello_ray() -> String {
-    let mut output = String::with_capacity(661_886);
-    let image_width = 256;
-    let image_height = 256;
-    output.push_str(format!("P3\n{} {}\n255\n", image_width, image_height).as_str());
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 384;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
 
-    for j in (0..(image_height - 1)).rev() {
-        print!("\rScanlines remaining: {}\n", j);
-        for i in 0..image_width {
-            let pixel = Color::new(
-                i as Num / (image_width - 1) as Num,
-                j as Num / (image_height - 1) as Num,
-                0.25,
-            );
-            output.push_str(pixel.ppm_fmt().as_str());
+    let mut ppm = String::with_capacity(661_886);
+    ppm.push_str(format!("P3\n{} {}\n255\n", image_width, image_height).as_str());
+
+    let origin = Point::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.25, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::unit_z(); // FIXME: negeative-z
+
+    for h in (0..(image_height - 1)).rev() {
+        print!("\rScanlines remaining: {}\n", h);
+        for w in 0..image_width {
+            let u = w as Num / (image_width - 1) as Num;
+            let v = h as Num / (image_height - 1) as Num;
+
+            // println!("h: {:?} - w: {:?} - u: {:?} - v: {:?}", h, w, u, v);
+
+            let r = Ray {
+                origin,
+                direction: lower_left_corner + (horizontal * u) + (vertical * v),
+            };
+            let pixel = ray_color(r);
+            ppm.push_str(ppm_fmt(pixel).as_str());
         }
     }
+
     println!("Done");
-    output
+    ppm
+}
+fn ray_color(ray: Ray) -> Color {
+    let unit_direction = ray.direction.unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+
+    (Color::one() * (1.0 - t)) + (Color::new(0.5, 0.7, 1.0) * t)
+    //^ white                     ^ blue
 }
 
-type Color = Vector3;
-
-// use std::fmt::{Display, Formatter, Result};
-impl Color {
-    fn ppm_fmt(&self) -> String {
-        format!(
-            "{} {} {}\n",
-            (255.999 * self.x) as i32,
-            (255.999 * self.y) as i32,
-            (255.999 * self.z) as i32
-        )
-    }
+fn ppm_fmt(pixel: Color) -> String {
+    format!(
+        "{} {} {}\n",
+        (255.999 * pixel.x) as i32,
+        (255.999 * pixel.y) as i32,
+        (255.999 * pixel.z) as i32
+    )
 }
 
-type Point = Vector3;
+type Color = Vec3;
 
+type Point = Vec3;
+
+#[derive(Debug)]
 pub struct Ray {
     pub origin: Point,
-    pub direction: Vector3,
+    pub direction: Vec3,
 }
 
 impl Ray {
